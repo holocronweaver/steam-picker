6f8d15e1a46aceed9f37cf82c170cb18cdd97e18
Jesse Johnson <holocronweaver@gmail.com>
2018-08-26 10:12:05 -0700

    Bump to version 0.1.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='steam-picker',
-    version='0.1.0',
+    version='0.1.2',
     author='Jesse Johnson',
     author_email='holocronweaver@gmail.com',
     packages=['steampicker'],
