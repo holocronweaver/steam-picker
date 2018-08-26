4396aa51136b026dc71b3d3938bafbcd9f594f28
Jesse Johnson <holocronweaver@gmail.com>
2018-08-26 12:14:58 -0700

    Bump to v0.1.4.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='steam-picker',
-    version='0.1.3',
+    version='0.1.4',
     author='Jesse Johnson',
     author_email='holocronweaver@gmail.com',
     packages=['steampicker'],
@@ -12,7 +12,6 @@ setup(
     license='LICENSE.txt',
     description='A Python 3 CLI for picking a random game from your Steam library!',
     # long_description=open('README.org').read(),
-    # Dependent packages (distributions)
     install_requires=[
         'appdirs',
         'pyyaml',
